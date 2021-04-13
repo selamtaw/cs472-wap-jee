6a1512a3c66d4d6ed3c01f9b749d5e25fcceeca4
Carlos Selamtaw Gebru <selamtawcarlos@gmail.com>
2021-04-12 21:42:38 -0500

    initial commit

---
+++ src/main/java/com/example/lab_12/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example.lab_12;
+
+import java.io.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello Java Servlet 4.0";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
